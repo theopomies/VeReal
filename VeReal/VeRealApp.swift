9764d764b546cf0aaddbdf57a2a14fd572dd8e06
Théo Pomies <theo.pomies@outlook.com>
2023-06-11 10:59:42 -0400

    Initial Commit

---
+++ VeReal/VeRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeRealApp.swift
+//  VeReal
+//
+//  Created by Théo Pomies on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct VeRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
